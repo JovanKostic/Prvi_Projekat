2eb7e7b8e48474e91f00f5d8ef212dcced31d99b
JovanKostic <48222063+JovanKostic@users.noreply.github.com>
2019-04-17 23:58:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import { filter, map, take, takeUntil, scan, debounceTime, switchMap, concatAll}
 import * as Rxjs from "rxjs";
 import {from as fromPromise} from "rxjs";
 const aukcija=new Aukcija();
-AukcijaService.get().then(artikal=>{
+/*AukcijaService.get().then(artikal=>{
+  console.log(artikal);
     aukcija.dodajArtikal(artikal);
     const auk_sek=document.getElementById("predmeti");
     aukcija.crtajArtikle(auk_sek);
-});
+});*/
 document.getElementById("postavi_predmet").onclick=(ev)=>{
   dodavanjePredmeta();
 }
-const ob=fromEvent(document.getElementById("registracija"),'click');
-ob.subscribe(ev=>console.log("reg"));    
-//testtt
+AukcijaService.dobavljanjeArtikalaIzBaze().then(artikal=>{
+  console.log(artikal.data);
+  aukcija.dodajArtikal(artikal.data);
+  const auk_sek=document.getElementById("predmeti");
+  aukcija.crtajArtikle(auk_sek);
+})
+AukcijaService.dobavljanjeKorisnikaIzBaze().then(korisnik=>{
+  console.log(korisnik.data);
+})
