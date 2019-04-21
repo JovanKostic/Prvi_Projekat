6fbfe0d1b6fac07ed9222400627eb40240944e24
JovanKostic <48222063+JovanKostic@users.noreply.github.com>
2019-04-21 17:30:49 +0200

    Update aukcija.js

--- src/klase/aukcija.js
+++ src/klase/aukcija.js
@@ -2,13 +2,15 @@ import {Artikal} from './artikal.js';
 import {Korisnik} from './korisnik';
 import {AukcijaService} from './aukcijaservice.js';
 import { range, interval, Observable, from, of, Subject, fromEvent} from "rxjs";
-import { filter, map, take, takeUntil, scan, debounceTime, switchMap, concatAll} from "rxjs/operators";
+import { filter, map, take, takeUntil, scan, debounceTime, switchMap, concatAll, create} from "rxjs/operators";
 import * as Rxjs from "rxjs";
 import {from as fromPromise} from "rxjs";
+import { fileURLToPath } from 'url';
 export class Aukcija{
     constructor(){
         this.listaKorisnika=[];
         this.listaArtikala=null;
+        this.listaPrijavljenihKorisnika=[];
         this.container=null;
     }
     dodajKorisnika(k){this.listaKorisnika=k;}
@@ -75,44 +77,18 @@ export class Aukcija{
             dugmeodustani.onclick=(ev)=>{
                 divpredmeta.hidden=false;
                 formazalicitaciju.hidden=true;
+                obs.unsubscribe(observer);
             }
             let observer=(x)=>{AukcijaService.azuriranjeCene(x.id,x.naziv,x.opis,unos.value);console.log(x.id,x.naziv,x.opis,unos.value);}
-            fromEvent(dugmelic,'click').pipe(
+            const obs=fromEvent(dugmelic,'click').pipe(
                             map(ev=>ev.target.value),
                             switchMap(ev=>fromPromise(
                                  fetch("http://localhost:3000/artikli/"+ev)
                                     .then(response=>response.json())
                                 )),
-                            filter(f=>f.cena<parseInt(unos.value))
+                            filter(f=>f.cena<parseInt(unos.value)),
+                            filter()
             ).subscribe(observer);
         }
-        
-        document.getElementById("registracija").onclick=(ev)=>{
-            const regModal=document.getElementById("regModal");
-            const inpIme=regModal.querySelector("input[name='ime']").value;
-            const inpPrezime=regModal.querySelector("input[name='prezime']").value;
-            const inpBrojLicneKarte=regModal.querySelector("input[name='brojlicnekarte']").value;
-            const inpKorisnickoIme=regModal.querySelector("input[name='korisnickoimeregistracija']").value;
-            const inpLozinka=regModal.querySelector("input[name='lozinkaregistracija']").value;
-            const k=new Korisnik(inpIme,inpPrezime,inpBrojLicneKarte,inpKorisnickoIme,inpLozinka);
-            console.log(k);
-            this.dodajKorisnika(k);
-            AukcijaService.dodavanjeKorisnikaUBazu(k);
-        }
-        document.getElementById("prijavljivanje").onclick=(ev)=>{
-            AukcijaService.dobavljanjeKorisnikaIzBaze().then(value=>{
-            this.dodajKorisnika(value.data);
-            this.listaKorisnika.forEach(k=>{
-            const loginModal=document.getElementById("loginModal");
-            const inpKorisnickoIme=loginModal.querySelector("input[name='korisnickoime']").value;
-            const inpLozinka=loginModal.querySelector("input[name='lozinka']").value;
-            if(k.korisnickoIme===inpKorisnickoIme && k.lozinka===inpLozinka)
-            {
-                k.prijavljen=true;console.log(k);
-            }
-            })
-        });
-    }
-        
     }
 }
