2a64ecb1476cffa2db943d556a7fcc7efcf2c880
JovanKostic <48222063+JovanKostic@users.noreply.github.com>
2019-04-24 23:57:36 +0200

    Update artikal.js

--- src/klase/artikal.js
+++ src/klase/artikal.js
@@ -1,8 +1,8 @@
+import {Korisnik} from './korisnik.js';
 export class Artikal{
     constructor(naziv,opis){
         this.naziv=naziv;
         this.opis=opis;
         this.cena=0;
-        this.korisniciKojiLicitiraju=[];
     }
 }
