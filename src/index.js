713cfc2a7ab429597a83ef6513b97e6fff34bd8c
JovanKostic <48222063+JovanKostic@users.noreply.github.com>
2019-04-25 21:21:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,6 @@ import * as Rxjs from "rxjs";
 import {from as fromPromise} from "rxjs";
 import { create } from 'domain';
 const aukcija=new Aukcija();
-AukcijaService.get().then(artikal=>{
-    aukcija.dodajArtikal(artikal);
-    const auk_sek=document.getElementById("predmeti");
-    aukcija.crtajArtikle(auk_sek);
-});
-document.getElementById("postavi_predmet").onclick=(ev)=>{
-  dodavanjePredmeta();
-}
 const ob=fromEvent(document.getElementById("registracija"),'click');
 ob.subscribe(ev=>console.log("reg"));    
 
