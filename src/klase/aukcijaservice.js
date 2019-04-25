27f9e878e7a3508fd7883940ebe89839ec4f5645
JovanKostic <48222063+JovanKostic@users.noreply.github.com>
2019-04-25 15:16:59 +0200

    Update aukcijaservice.js

--- src/klase/aukcijaservice.js
+++ src/klase/aukcijaservice.js
@@ -15,12 +15,13 @@ export class AukcijaService{
         const axios = require('axios');
         return axios.get('http://localhost:3000/korisnici');
     }
-    static azuriranjeCene(id,naziv,opis,cena){
+    static azuriranjeCeneIKupca(id,naziv,opis,cena,kupac){
         const axios = require('axios');
         axios.put('http://localhost:3000/artikli/'+id, {
             naziv:naziv,
             opis:opis,
-            cena:cena
+            cena:cena,
+            kupac:kupac
         }).then(resp => {
             console.log(resp.data);
         }).catch(error => {
@@ -35,7 +36,8 @@ export class AukcijaService{
             brojLicneKarte:korisnik.brojLicneKarte,
             korisnickoIme:korisnik.korisnickoIme,
             lozinka:korisnik.lozinka,
-            prijavljen:korisnik.prijavljen
+            prijavljen:korisnik.prijavljen,
+            predmet:korisnik.predmet
         }).then(resp => {
             console.log(resp.data);
         }).catch(error => {
@@ -50,11 +52,36 @@ export class AukcijaService{
             brojLicneKarte:korisnik.brojLicneKarte,
             korisnickoIme:korisnik.korisnickoIme,
             lozinka:korisnik.lozinka,
-            prijavljen:korisnik.prijavljen
+            prijavljen:korisnik.prijavljen,
+            predmet:""
         }).then(resp => {
             console.log(resp.data);
         }).catch(error => {
     console.log(error);
         });
+    }
+    static dodavanjePoslatihPredmeta(ime,prezime,brojlicnekarte,ziroracun,adresaisporuke,nazivpredmeta){
+        const axios = require('axios');
+        axios.post('http://localhost:3000/poslatiPredmeti', {
+            ime:ime,
+            prezime:prezime,
+            brojLicneKarte:brojlicnekarte,
+            ziroracun:ziroracun,
+            adresaisporuke:adresaisporuke,
+            nazivpredmeta
+        }).then(resp => {
+            console.log(resp.data);
+        }).catch(error => {
+    console.log(error);
+        });
+    }
+    static brisanjePoslatihPredmeta(id){
+        const axios = require('axios');
+        axios.delete('http://localhost:3000/artikli/'+id)
+        .then(resp => {
+            console.log(resp.data)
+        }).catch(error => {
+            console.log(error);
+        });   
     }
 }
